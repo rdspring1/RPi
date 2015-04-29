1a54b35abab8cc9e55004b2662dbb13fc4037a62
Ryan DS <rdspring1@gmail.com>
2015-04-28 22:02:36 -0500

    Expanded basic image detection to handle multiple objects

--- opencv_communication/basic_image_detection.h
+++ opencv_communication/basic_image_detection.h
@@ -9,37 +9,44 @@
 class BasicImageDetection : public InterpreterBase
 {
 	public:
-		BasicImageDetection(ObjectDetector& d) : InterpreterBase(d), moving_avg(MAX_THRESHOLD) {}
+		BasicImageDetection(ObjectDetector& d) : InterpreterBase(d), object_tracker(num_objects(), Mavg(MAX_THRESHOLD)) {}
 
 		virtual IReport detect(Mat& img_scene)
 		{
 			processScene(img_scene);
-			std::vector< DMatch > local_matches;
-			moving_avg.update(processObject(0, local_matches));
+			for(unsigned idx = 0; idx < num_objects(); ++idx)
+			{
+				std::vector< DMatch > local_matches;
+				object_tracker[idx].update(processObject(object_library().object_idx[idx], local_matches));
+				debugImage(object_library().object_idx[idx], local_matches);
+			}
 
 			IReport ir;
-			if(moving_avg.count() >= MATCH_THRESHOLD)
-			{	
-				//std::cout << "MATCH DETECTED: " << match_count << std::endl;
-				ir.objects.push_back(true);
-				ir.object_confidence.push_back(1);
-				ir.images.push_back(true);
-				ir.image_confidence.push_back(1);
-			}
-			else
+			for(unsigned idx = 0; idx < num_objects(); ++idx)
 			{
-				//std::cout << "NO MATCH DETECTED: " << match_count << std::endl;
-				ir.objects.push_back(false);
-				ir.object_confidence.push_back(0);
-				ir.images.push_back(false);
-				ir.image_confidence.push_back(0);
+				if(object_tracker[idx].count() >= MATCH_THRESHOLD)
+				{	
+					//std::cout << "MATCH DETECTED: " << match_count << std::endl;
+					ir.objects.push_back(true);
+					ir.object_confidence.push_back(1);
+					ir.images.push_back(true);
+					ir.image_confidence.push_back(1);
+				}
+				else
+				{
+					//std::cout << "NO MATCH DETECTED: " << match_count << std::endl;
+					ir.objects.push_back(false);
+					ir.object_confidence.push_back(0);
+					ir.images.push_back(false);
+					ir.image_confidence.push_back(0);
+				}
 			}
 			return ir;
 		}
 	private:
 		const double MAX_THRESHOLD = 10;
 		const double MATCH_THRESHOLD = 1;
-		Mavg moving_avg;
+		std::vector<Mavg> object_tracker;
 };
 
 #endif /* BASIC_IMAGE_DETECTION_H */
